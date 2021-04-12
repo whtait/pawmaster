fbe61357d80fe855fc22beeb627ca463796e1ee9
Warren Tait <warrenhtait@gmail.com>
2021-04-12 14:57:06 -0400

    big brain shit

--- infra/deploy-infra.ps1
+++ infra/deploy-infra.ps1
@@ -3,8 +3,8 @@ $region='us-east-1'
 $env:AWS_DEFAULT_REGION=$region
 $env:AWS_REGION=$region
 
-$env:AWS_SECRET_ACCESS_KEY = "fYdFA47ZX4W0/1ql4JzvLAFHS0EGwvN1t7ey4xgC"
-$env:AWS_ACCESS_KEY_ID = "AKIAVOV5RWKZN46N5Z76"
+$env:AWS_SECRET_ACCESS_KEY = ""
+$env:AWS_ACCESS_KEY_ID = ""
 
 aws cloudformation deploy `
   --region ${region} `
