8017b09a9144f9dfe94c6b46413abda3c8bead2c
Warren Tait <warrenhtait@gmail.com>
2021-04-12 14:58:43 -0400

    Fuck

--- infra/deploy-infra.ps1
+++ infra/deploy-infra.ps1
@@ -3,9 +3,6 @@ $region='us-east-1'
 $env:AWS_DEFAULT_REGION=$region
 $env:AWS_REGION=$region
 
-$env:AWS_SECRET_ACCESS_KEY = ""
-$env:AWS_ACCESS_KEY_ID = ""
-
 aws cloudformation deploy `
   --region ${region} `
   --template-file cfn/infra.yml `
